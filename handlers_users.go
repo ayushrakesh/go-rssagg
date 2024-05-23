bc82f9d1f9375a207797d4db92bdfe8e0a6cccd7
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2024-05-23 17:05:26 +0530

    SQL implemented

---
+++ handlers_users.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ayushrakesh/go-rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		Name string `json:"name"`
+	}
+
+	params := parameters{}
+
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON - %s\n", err))
+		return
+	}
+
+	user, er := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if er != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create user %s\n", er))
+		return
+	}
+
+	respondWithJSON(w, 200, user)
+}
--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/ayushrakesh/go-rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
-
 	if portString == "" {
 		log.Fatal("Port is not found in the environment")
 	}
 	fmt.Println("Port: ", portString)
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("Database not found!")
+	}
+
+	conn, er := sql.Open("postgres", dbUrl)
+	fmt.Println(conn)
+	if er != nil {
+		log.Fatal("Can't connect to the database!")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
 
 	router := chi.NewRouter()
 
@@ -36,8 +57,10 @@ func main() {
 	)
 
 	v1Router := chi.NewRouter()
+
 	v1Router.Get("/readiness", handlerReadiness)
 	v1Router.Get("/error", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
